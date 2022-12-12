dcced3c21b86de32e5c75022ca56ff48e0356ca0
Ibukun <jonathan4lyf@gmail.com>
2022-12-12 07:20:51 -0800

    small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- Entry point here
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z';	c++)
+	{
+	putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
