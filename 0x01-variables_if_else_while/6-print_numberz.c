468e36ebc8b227b1b13184e8ef2fdf972ec0b08c
Ibukun <jonathan4lyf@gmail.com>
2022-12-12 14:18:43 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int numberz;
+
+	for (numberz = 48; numberz <= 57; numberz++)
+
+	{
+
+		putchar (numberz);
+
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
