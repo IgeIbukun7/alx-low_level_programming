39f67a364e048be29bdec47d57f1b481a8e34c0b
Ibukun <jonathan4lyf@gmail.com>
2022-12-12 14:41:24 -0800

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char z;
+
+	for (z = 122 ; z >= 97; z--)
+
+	{
+
+		putchar(z);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
