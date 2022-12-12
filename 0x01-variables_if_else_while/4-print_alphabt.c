d299f928921e263e6eac2dc65ecd8fc00471d249
Ibukun <jonathan4lyf@gmail.com>
2022-12-12 13:42:40 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabt;
+
+	for (alphabt = 'a'; alphabt <= 'z'; alphabt++)
+
+	{
+
+	if (alphabt == 'q')
+	continue;
+
+	else if (alphabt == 'e')
+	continue;
+
+	putchar(alphabt);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
