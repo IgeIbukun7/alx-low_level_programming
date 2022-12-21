7e9343ae2a3581924fc0e2e3490c6337d8862af9
Ibukun <jonathan4lyf@gmail.com>
2022-12-21 12:53:47 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,6 +15,7 @@ int _strcmp(char *s1, char *s2)
 	int i = 0, r;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
+	{
 		if (s1[i] != s2[i])
 		{
 			r = s1[i] - s2[i];
@@ -25,6 +26,6 @@ int _strcmp(char *s1, char *s2)
 			r = s1[i] - s2[i];
 		}
 	i++;
-}
-return (r);
+	}
+	return (r);
 }
