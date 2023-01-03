eeb41c81727b5bd3be702dd010bada4780d50a1f
Ibukun <jonathan4lyf@gmail.com>
2023-01-03 04:19:46 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Returns pointer to first occurence in string
+ *
+ * in the string s
+ *
+ * @s: string to search
+ *
+ * @accept: characters to look for
+ *
+ * Return: pointer to first occurrence of accept in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+
+{
+	char *ptr = accept;
+
+	while (*s != 0)
+	{
+		if (*s == *ptr && *ptr != 0)
+			return (s);
+		ptr++;
+		if (*ptr == 0)
+		{
+			ptr = accept;
+			s++;
+		}
+		return (0);
+	}
+	s++;
+}
