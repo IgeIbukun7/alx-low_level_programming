96bfa3e11f9ff64b736aa8e60061258dba1fd359
Ibukun <jonathan4lyf@gmail.com>
2023-01-10 05:23:44 -0800

    initial commit

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - this function checks for a digit
+ * @c: int to be checked
+ *
+ * Return: 1 if c is a digit, else 0
+ */
+
+int _isdigit(int c)
+
+{
+	return (c >= '0' && c <= '9');
+}
